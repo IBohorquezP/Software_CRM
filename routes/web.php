99b10bead651183577ef4d1500b5de7d95222d00
Ismael <pradoismael50@gmail.com>
2025-01-24 09:53:35 -0400

    Permisos

--- routes/web.php
+++ routes/web.php
@@ -36,28 +36,33 @@ Route::get('/Planificacion', function () {
 });
 
 // //Ruta Clientes
+
+Route::group(['middleware' => ['can:Clientes.create', 'can:Clientes.edit', 'can:Clientes.destroy']], function () {
+    Route::get('/Clientes/create', [ClienteController::class, 'create'])->name('Clientes.create'); // Mostrar formulario de creación
+    Route::post('/Clientes', [ClienteController::class, 'store'])->name('Clientes.store');        // Guardar nuevo cliente
+    Route::get('/Clientes/{id_cliente}/edit', [ClienteController::class, 'edit'])->name('Clientes.edit'); // Formulario de edición
+    Route::put('/Clientes/{id_cliente}', [ClienteController::class, 'update'])->name('Clientes.update');  // Actualizar cliente
+    Route::delete('/Clientes/{id_cliente}', [ClienteController::class, 'destroy'])->name('Clientes.destroy'); // Eliminar cliente
+});
 Route::get('/Clientes', [ClienteController::class, 'index'])->name('Clientes.index');         // Mostrar lista de clientes
-Route::get('/Clientes/create', [ClienteController::class, 'create'])->name('Clientes.create'); // Mostrar formulario de creación
-Route::post('/Clientes', [ClienteController::class, 'store'])->name('Clientes.store');        // Guardar nuevo cliente
 Route::get('/Clientes/{id_cliente}', [ClienteController::class, 'show'])->name('Clientes.show'); // Mostrar cliente específico
-Route::get('/Clientes/{id_cliente}/edit', [ClienteController::class, 'edit'])->name('Clientes.edit'); // Formulario de edición
-Route::put('/Clientes/{id_cliente}', [ClienteController::class, 'update'])->name('Clientes.update');  // Actualizar cliente
-Route::delete('/Clientes/{id_cliente}', [ClienteController::class, 'destroy'])->name('Clientes.destroy'); // Eliminar cliente
 
 Route::controller(TecnicoController::class)->group(function () {
 // //Ruta Tecnicos
-Route::get('/Tecnicos', 'index')->name('Tecnicos.index');
-Route::get('/Tecnicos/create', 'create')->name('Tecnicos.create');
-Route::post('/Tecnicos', 'store')->name('Tecnicos.store');
-Route::get('/Tecnicos/{id_tecnico}', 'show')->name('Tecnicos.show');
-Route::get('/Tecnicos/{id_tecnico}/edit', 'edit')->name('Tecnicos.edit');
-Route::put('/Tecnicos/{id_tecnico}', 'update')->name('Tecnicos.update');
-Route::delete('/Tecnicos/{id_tecnico}', 'destroy')->name('Tecnicos.destroy');
 
+Route::group(['middleware' => ['can:Tecnicos.create', 'can:Tecnicos.edit', 'can:Tecnicos.destroy']], function () {
+    Route::get('/Tecnicos/create', 'create')->name('Tecnicos.create');
+    Route::post('/Tecnicos', 'store')->name('Tecnicos.store');
+    Route::get('/Tecnicos/{id_tecnico}/edit', 'edit')->name('Tecnicos.edit');
+    Route::put('/Tecnicos/{id_tecnico}', 'update')->name('Tecnicos.update');
+    Route::delete('/Tecnicos/{id_tecnico}', 'destroy')->name('Tecnicos.destroy');
+});
+Route::get('/Tecnicos', 'index')->name('Tecnicos.index');
+Route::get('/Tecnicos/{id_tecnico}', 'show')->name('Tecnicos.show');
+Route::get('/Servicio/Tecnicos/{id_servicio_tecnico}', 'showServicioTecnicos')->name('Tecnicos.showServicioTecnicos');
  //Rutas AsignarBahias
  Route::get('/AsignarTecnicos/{id_servicio}', 'asignarTecnicos')->name('Tecnicos.asignarTecnicos');
  Route::post('/AsignarTecnicos/store', 'attachServicio')->name('Tecnicos.attachServicio');
- Route::get('/Servicio/Tecnicos/{id_servicio_tecnico}', 'showServicioTecnicos')->name('Tecnicos.showServicioTecnicos');
  Route::get('/Servicio/{id_servicio}/Tecnicos/{id_tecnico}/edit', 'editServicioTecnicos')->name('Tecnicos.editServicioTecnicos');
  Route::put('/Servicio/{id_servicio}/Tecnicos/{id_tecnico}', 'updateServicioTecnicos')->name('Tecnicos.updateServicioTecnicos');
  Route::delete('/Servicio/{id_servicio}/Tecnicos/{id_tecnico}', 'destroyServicioTecnicos')->name('Tecnicos.destroyServicioTecnicos');
@@ -75,50 +80,57 @@ Route::delete('/Etapas/{id_etapa}', [EtapaController::class, 'destroy'])->name('
 
 
 
-Route::controller(BahiaController::class)->group(function () {
-    //Ruta Bahia
+Route::controller(BahiaController::class)->group(function () { 
+    Route::group(['middleware' => ['can:Bahias.create', 'can:Bahias.edit', 'can:Bahias.destroy']], function () {
+        //Ruta Bahia
+        Route::get('/Bahias/create', 'create')->name('Bahias.create');
+        Route::post('/Bahias', 'store')->name('Bahias.store');
+        Route::get('/Bahias/{id_bahia}/edit', 'edit')->name('Bahias.edit');
+        Route::put('/Bahias/{id_bahia}', 'update')->name('Bahias.update');
+        Route::delete('/Bahias/{id_bahia}', 'destroy')->name('Bahias.destroy');
+        
+        //Rutas AsignarBahias
+        Route::get('/AsignarBahias/{id_servicio}', 'asignarBahias')->name('Bahias.asignarBahias');
+        Route::post('/AsignarBahias/store', 'attachServicio')->name('Bahias.attachServicio');
+        Route::get('/Servicio/{id_servicio}/Bahias/{id_bahia}/edit', 'editServicioBahias')->name('Bahias.editServicioBahias');
+        Route::put('/Servicio/{id_servicio}/Bahias/{id_bahia}', 'updateServicioBahias')->name('Bahias.updateServicioBahias');
+        Route::delete('/Servicio/{id_servicio}/Bahias/{id_bahia}', 'destroyServicioBahias')->name('Bahias.destroyServicioBahias');
+    });
     Route::get('/Bahias', 'index')->name('Bahias.index');
-    Route::get('/Bahias/create', 'create')->name('Bahias.create');
-    Route::post('/Bahias', 'store')->name('Bahias.store');
     Route::get('/Bahias/{id_bahia}', 'show')->name('Bahias.show');
-    Route::get('/Bahias/{id_bahia}/edit', 'edit')->name('Bahias.edit');
-    Route::put('/Bahias/{id_bahia}', 'update')->name('Bahias.update');
-    Route::delete('/Bahias/{id_bahia}', 'destroy')->name('Bahias.destroy');
-
-    //Rutas AsignarBahias
-    Route::get('/AsignarBahias/{id_servicio}', 'asignarBahias')->name('Bahias.asignarBahias');
-    Route::post('/AsignarBahias/store', 'attachServicio')->name('Bahias.attachServicio');
     Route::get('/Servicio/Bahias/{id_servicio_bahia}', 'showServicioBahias')->name('Bahias.showServicioBahias');
-    Route::get('/Servicio/{id_servicio}/Bahias/{id_bahia}/edit', 'editServicioBahias')->name('Bahias.editServicioBahias');
-    Route::put('/Servicio/{id_servicio}/Bahias/{id_bahia}', 'updateServicioBahias')->name('Bahias.updateServicioBahias');
-    Route::delete('/Servicio/{id_servicio}/Bahias/{id_bahia}', 'destroyServicioBahias')->name('Bahias.destroyServicioBahias');
 });
 
 // //Ruta Repuestos
+Route::get('/Servicio/Repuestos/{id_servicio}', [RepuestoController::class, 'show'])->name('Repuestos.show');
+Route::group(['middleware' => ['can:Repuestos.create', 'Repuestos.edit', 'Repuestos.destroy']], function () {
 Route::get('/Repuestos/{id_servicio}', [RepuestoController::class, 'create'])->name('Repuestos.create');
 Route::post('/Servicio/Repuestos', [RepuestoController::class, 'store'])->name('Repuestos.store');
-Route::get('/Servicio/Repuestos/{id_servicio}', [RepuestoController::class, 'show'])->name('Repuestos.show');
 Route::get('/Servicio/{id_servicio}/Repuestos/{id_repuesto}/edit', [RepuestoController::class, 'edit'])->name('Repuestos.edit');
 Route::put('/Servicio/{id_servicio}/Repuestos/{id_repuesto}', [RepuestoController::class, 'update'])->name('Repuestos.update');
 Route::delete('/Servicio/{id_servicio}/Repuestos/{id_repuesto}', [RepuestoController::class, 'destroy'])->name('Repuestos.destroy');
+});
 
 // //Ruta Externos
+Route::get('/Servicio/Externos/{id_servicio}', [ExternoController::class, 'show'])->name('Externos.show');
+Route::group(['middleware' => ['can:Externos.create', 'can:Externos.edit', 'can:Externos.destroy']], function () {
 Route::get('/Externos/{id_servicio}', [ExternoController::class, 'create'])->name('Externos.create');
 Route::post('/Servicio/Externos', [ExternoController::class, 'store'])->name('Externos.store');
-Route::get('/Servicio/Externos/{id_servicio}', [ExternoController::class, 'show'])->name('Externos.show');
 Route::get('/Servicio/{id_servicio}/Externos/{id_externo}/edit', [ExternoController::class, 'edit'])->name('Externos.edit');
 Route::put('/Servicio/{id_servicio}/Externos/{id_externo}', [ExternoController::class, 'update'])->name('Externos.update');
 Route::delete('/Servicio/{id_servicio}/Externos/{id_externo}', [ExternoController::class, 'destroy'])->name('Externos.destroy');
+});
 
 // //Ruta Servicios
+Route::group(['middleware' => ['can:Servicios.create', 'can:Servicios.edit', 'can:Servicios.destroy']], function () {
+    Route::get('/Servicios/create', [ServicioController::class, 'create'])->name('Servicios.create');
+    Route::post('/Servicios', [ServicioController::class, 'store'])->name('Servicios.store');
+    Route::get('/Servicios/{id_servicio}/edit', [ServicioController::class, 'edit'])->name('Servicios.edit');
+    Route::put('/Servicios/{id_servicio}', [ServicioController::class, 'update'])->name('Servicios.update');
+    Route::delete('/Servicios/{id_servicio}', [ServicioController::class, 'destroy'])->name('Servicios.destroy');
+});
 Route::get('/Servicios', [ServicioController::class, 'index'])->name('Servicios.index');
-Route::get('/Servicios/create', [ServicioController::class, 'create'])->name('Servicios.create');
-Route::post('/Servicios', [ServicioController::class, 'store'])->name('Servicios.store');
 Route::get('/Servicios/{id_servicio}', [ServicioController::class, 'show'])->name('Servicios.show');
-Route::get('/Servicios/{id_servicio}/edit', [ServicioController::class, 'edit'])->name('Servicios.edit');
-Route::put('/Servicios/{id_servicio}', [ServicioController::class, 'update'])->name('Servicios.update');
-Route::delete('/Servicios/{id_servicio}', [ServicioController::class, 'destroy'])->name('Servicios.destroy');
-
 // Ruta Reporte PDF
 Route::get('/Servicios/{id_servicio}/Reporte', [PdfController::class, 'reporteServicio'])->name('Servicios.reporteServicio');
 Route::get('/Servicios/{id_servicio}/Bahias /{id_bahia}/Reporte', [PdfController::class, 'reporteServicioBahias'])->name('Servicios.reporteServicioBahias');
